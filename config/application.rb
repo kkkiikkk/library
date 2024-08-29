63c6411caa422db3790a94ed824989040778fa17
kkkiikkk <ruddnovskiy@gmail.com>
2024-08-29 13:19:03 +0300

    feat: add mongodb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +13,7 @@ module LibraryExercise
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    # config.active_record.raise_in_transactional_callbacks = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
