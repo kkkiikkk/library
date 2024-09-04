dd3c2a322cb869a2b75a3977decc53c3e05c6900
kkkiikkk <ruddnovskiy@gmail.com>
2024-09-04 13:01:34 +0300

    feat: add navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ActionController::Base 
+class HomeController < ApplicationController 
   def index
     relation = Book.all
     @books = PopularBooksQuery.new.call(relation)
