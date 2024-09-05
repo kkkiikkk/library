0c00e7a063e5c1507bc1e6eabf05d70e92f99ff3
kkkiikkk <ruddnovskiy@gmail.com>
2024-09-05 21:46:25 +0300

    feat: add filter and sorting

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,31 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+
     if params[:search].present?
-      @books = Book.where(name: /#{params[:search]}/i).page(params[:page]).per(5)
-    else
-      @books = Book.all.page(params[:page]).per(5)
+      @books = @books.where(name: /#{params[:search]}/i)
     end
+
+    if params[:status].present?
+      @books = @books.where(status: params[:status])
+    end
+
+    if params[:author].present?
+      @books = @books.where(author: /#{params[:author]}/i)
+    end
+
+    case params[:sort]
+    when 'name'
+      @books = @books.order_by(name: :asc)
+    when 'author'
+      @books = @books.order_by(author: :asc)
+    when 'likes'
+      @books = @books.sort_by { |book| book.likes_count }
+    else
+      @books = @books.order_by(created_at: :desc)
+    end
+
+    @books = @books.page(params[:page]).per(5)
     flash.now[:notice] = 'No books found' if @books.empty?
   end
 
